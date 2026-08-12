5c35479622bc7f38c0594d6de60b46f1abdef594
Harish Soni <soniharish029@gmail.com>
2026-08-12 23:25:56 +0530

    solved the question in both manner

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # Given an integer n, build a right-angled triangle made of stars and return it as an array of strings, where each string is one row of the triangle.
-    
+def printPattern():
+    for i in range(0,5):
+        for j in range(0,i):
+            print(i,end=' ')
+        print()
+printPattern()
