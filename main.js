8d01d7121943a861d4f8a76227c3857703094a40
Harish Soni <soniharish029@gmail.com>
2026-08-31 21:38:21 +0530

    added cmment to the unecessary code

--- main.js
+++ main.js
@@ -115,18 +115,18 @@
 // floyd(5)
 
 // Given an integer n, return true if n is a prime number and false otherwise.
-const checkPrime = (n)=>{
-    let isPrime=true
-for(i=2;i<n;i++){
-    if(n%i===0){
-       isPrime=false
-    }
-}
-if(isPrime){
-    console.log('prime')
-}else{
-    console.log('not prime')
-}
-}
+// const checkPrime = (n)=>{
+//     let isPrime=true
+// for(i=2;i<n;i++){
+//     if(n%i===0){
+//        isPrime=false
+//     }
+// }
+// if(isPrime){
+//     console.log('prime')
+// }else{
+//     console.log('not prime')
+// }
+// }
 
-checkPrime(17)
+// checkPrime(17)
--- main.py
+++ main.py
@@ -81,14 +81,14 @@
 # floyd(3)
 
 # Given an integer n, return true if n is a prime number and false otherwise.
-def checkPrime(n):
-    isPrime=True
-    for i in range(2,n):
-        if(n%i==0):
-            isPrime=False
-    if(isPrime):
-        print(True)
-    else:
-        print(False)
+# def checkPrime(n):
+#     isPrime=True
+#     for i in range(2,n):
+#         if(n%i==0):
+#             isPrime=False
+#     if(isPrime):
+#         print(True)
+#     else:
+#         print(False)
 
-checkPrime(10)
+# checkPrime(10)
